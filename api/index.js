aeeb9065412fecb74a3349eed2918090175fe1ce
Leven-mok <65766938+Leven-mok@users.noreply.github.com>
2022-02-17 12:40:44 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@ const { createRequestHandler } = require("@remix-run/vercel");
 
 module.exports = createRequestHandler({
   build: require("./_build")
-  
-import { Route } from "react-router-dom";
 
 function BlogPost() {
   return (
